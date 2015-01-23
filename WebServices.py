ce59e74342249efa8cabf9ad9fbdc292f30b3547
mewsha <users.noreply.github.com>
2015-01-22 21:10:11 -0800

    update WebServices module

--- WebServices.py
+++ WebServices.py
@@ -4,9 +4,23 @@ import urllib2
 import JSON
 
 class WebServices(object):
+	URL = "http://api.petfinder.com/"
+	apiModule = "pet.getRandom"
+	apiKey = "?key=33faa4e89fd6859ab68709e31dd139b5"
+	apiFormat = "&format=json"
+	apiOutput = "&output=basic"
+	
+	URI = URL+apiModule+apiKey+apiFormat+apiOutput
 	
 	def __init__(self):
 		pass
 		
-		
+	def requestPetData(self):
+		pass
 	
+	def interpretJSONData(self, data):
+		pass
+		
+	def getPet():
+		pass
+		
