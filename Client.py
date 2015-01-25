2ff58de4288182e2d24d2c7221c4279f7d566925
Mewsha <mewsha@users.noreply.github.com>
2015-01-25 01:42:45 -0800

    height is fixed

--- Client.py
+++ Client.py
@@ -15,31 +15,46 @@ class CuteOrNot(QDialog):
 		QDialog.__init__(self)
 		self.webServ = WebServices.WebServices()
 		self.showPets()
+		self.setWindowTitle('Cute or Not?')
 
 	def layoutLeaderboard(self):
+		"""
+		Created and displays UI for the leaderboard view
+		"""
 		layout = QGridLayout()
 		label = QLabel("images here")
 		label2 = QLabel("scroll here")
+		backButton = QPushButton("Back to Pets")
 
-		layout.addWidget(label)
-		layout.addWidget(label2)
-		
+		layout.addWidget(label, 0, 1)
+		layout.addWidget(label2, 0 , 2)
+		layout.addWidget(backButton, 1, 0)
+				
 		self.setLayout(layout)
+		
+		backbButton.clicked.connect(self.showPets)
 
 	def layoutPets(self, pet1Data, pet2Data):
+		"""
+		Creates and displays UI for the Pet view. 
+		"""
+		pet1Image = QLabel(self)
+		pet2Image = QLabel(self)
+		imgpolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+		pet1Image.setSizePolicy(imgpolicy)
+		pet2Image.setSizePolicy(imgpolicy)
+			
 		layout = QGridLayout()
 		
 		label = QLabel("Cute Or Not?")
 		pxmap1 = QPixmap(pet1Data)
-		print pxmap1
 		pxmap2 = QPixmap(pet2Data)
-		print pxmap2
-		pet1Image = QLabel(self)
-		pet2Image = QLabel(self)
 		pet1Image.setPixmap(pxmap1)
 		pet2Image.setPixmap(pxmap2)
-		pet1Button = QPushButton("Cuter")
-		pet2Button = QPushButton("Cutest")
+		pet1Image.setFixedHeight(300)
+		pet2Image.setFixedHeight(300)
+		pet1Button = QPushButton("I'm Cuter!")
+		pet2Button = QPushButton("I'm Cutest!")
 		exitButton = QPushButton("Close")
 		leaderButton = QPushButton("Leaderboard")
 		
@@ -52,27 +67,33 @@ class CuteOrNot(QDialog):
 		layout.addWidget(leaderButton, 3, 1)		
 		
 		self.setLayout(layout)
-		self.setWindowTitle('Cute or Not?')
+		
 		exitButton.clicked.connect(self.close)
 		
+		
 	def showPets(self):
+		"""
+		Show the pet view in the window
+		"""
 		pet1Data = self.webServ.getAPet()
 		pet2Data = self.webServ.getAPet()
 		pet1ImgURL = pet1Data['photo']['$t']
 		pet2ImgURL = pet2Data['photo']['$t']
-		pet1ImgData = self.downloadImage(pet1ImgURL, "pet1.jpg")
-		pet2ImgData = self.downloadImage(pet2ImgURL, "pet2.jpg")
+		pet1ImgData = self.downloadImage(pet1ImgURL)
+		pet2ImgData = self.downloadImage(pet2ImgURL)
 		self.layoutPets(pet1ImgData, pet2ImgData)
 		
-	def downloadImage(self, url, filename):
+	def downloadImage(self, url):
+		"""
+		Download the pet image from a given url.
+		
+		Returns:
+		QImage object with the pet's image
+		"""
 		imagefile = QImage()
-		#save_spot = '~/Downloads/'+ filename
-		#response = urllib2.Request(url)
 		urldata = urllib2.urlopen(url)
 		imagedata = urldata.read()
 		imagefile.loadFromData(imagedata)
-		#urllib.urlretrieve(url, save_spot)
-		#image = None
 		return imagefile
 		
 app = QApplication(sys.argv)
